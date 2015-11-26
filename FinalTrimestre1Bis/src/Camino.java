e4bb74eeef8ea6625cf3e87c7174080de037b63b
fernando.sanchez <fsm_031@hotmail.com>
2015-11-26 13:31:13 +0100

    PRIMER COMMIT: 1:30 HORAS

--- FinalTrimestre1Bis/src/Camino.java
+++ FinalTrimestre1Bis/src/Camino.java
@@ -1,49 +1,157 @@
+import java.util.Scanner;
+
 /**
  * @author 
  * @date 26/11/2015
  */
 
-/**	Camino:
-	- Es la única clase que tiene main y desde la que se deben lanzar todos los hilos. 
-  	- Al comenzar el camino se preguntará por teclado el número de Peregrinos que comienzan el camino 
-	y se lanzará un hilo por cada uno de ellos.
-	- También se creará un hilo para representar la Barca y otro para el Barquero.
-	- El lago del camino sólo se puede atravesar en barca.
-	- El programa informará por consola quién llega al lago, quién viaja en barca y quién llega a Santiago.
-	- El programa termina cuando llegan todos los peregrinos.
+/**
+ * Camino: - Es la única clase que tiene main y desde la que se deben lanzar
+ * todos los hilos. - Al comenzar el camino se preguntará por teclado el número
+ * de Peregrinos que comienzan el camino y se lanzará un hilo por cada uno de
+ * ellos. - También se creará un hilo para representar la Barca y otro para el
+ * Barquero. - El lago del camino sólo se puede atravesar en barca. - El
+ * programa informará por consola quién llega al lago, quién viaja en barca y
+ * quién llega a Santiago. - El programa termina cuando llegan todos los
+ * peregrinos.
  */
 public class Camino {
+	static int numperegrinos;
+	static Scanner in = new Scanner(System.in);
+
 	public static void main(String[] args) {
+
+		System.out.println("ESCRIBE EL NUMERO DE PEREGRINOS");
+		numperegrinos = in.nextInt();
+
+		Barca barca = new Barca();
+
+		Thread Peregrino;
+
+		for (int i = 0; i < numperegrinos; i++) {
+			Peregrino = new Peregrino(barca, i);
+			System.out.println("EL PEREGRINO Nº: " + i
+					+ "ESTÁ LISTO PARA ERMPEZAR A ANDAR");
+			Peregrino.start();
+
+		}
+
+		Thread Barquero = new Barquero(barca);
+		Barquero.start();
+
 	}
 }
 
-/** Peregrino:
-	- Cada peregrino está identificado con un número, que se utilizará para los mensajes por pantalla.
-	- Desde que comienza el trayecto, tarda un tiempo aleatorio (entre 1 y 4 segundos) en atravesar el camino que le lleva hasta el Lago.
-	- Si cuando llega la barca está libre, se sube en ella (informando también por consola) y comienza a pasar el lago. En caso contrario, deberá esperar.
-	- Al finalizar el trayecto en barca continua su camino, tardando un tiempo aleatorio entre  0,5 - 1 segundos en llegar a Santiago.
+// --------------------------------------------------------------------------------------------------------------------------------------//
+
+/**
+ * Peregrino: - Cada peregrino está identificado con un número, que se utilizará
+ * para los mensajes por pantalla. - Desde que comienza el trayecto, tarda un
+ * tiempo aleatorio (entre 1 y 4 segundos) en atravesar el camino que le lleva
+ * hasta el Lago. - Si cuando llega la barca está libre, se sube en ella
+ * (informando también por consola) y comienza a pasar el lago. En caso
+ * contrario, deberá esperar. - Al finalizar el trayecto en barca continua su
+ * camino, tardando un tiempo aleatorio entre 0,5 - 1 segundos en llegar a
+ * Santiago.
  */
-class Peregrino {
-	
+class Peregrino extends Thread {
+
+	private Barca barca;
+	private int id;
+
+	public Peregrino(Barca barca, int id) {
+		this.barca = barca;
+		this.id = id;
+	}
+
+	public void run() {
+		try {
+			Thread.sleep(arg0);
+			
+		} catch (Exception e) {
+			
+		}
+
+	}
+
 }
 
+// --------------------------------------------------------------------------------------------------------------------------------------//
 
-/** Barca:
-	- Debe ser el objeto compartido entre todos los hilos.
-	- Sólo tiene una plaza libre (además de la del propio barquero).
-	- Cuando sube un peregrino en su interior, se anota su identificador.
+/**
+ * Barquero: - Es quien conduce la Barca y debe esperar a que haya algún
+ * peregrino para comenzar su viaje. - En el trayecto de ida tarda 0,5 segundos
+ * y al terminar informa por consola del peregrino que ha llegado al otro lado
+ * del lago. - En el trayecto de vuelta tarda 0,1 segundos, ya que vuelve vacío.
+ */
+class Barquero extends Thread {
+
+	private Barca barca;
+
+	public Barquero(Barca barca) {
+		this.barca = barca;
+	}
+
+	public void run() {
+
+		
+		try {
+			Thread.sleep(500);
+			barca.empezarviaje();
+			Thread.sleep(500);
+			System.out.println("SE HA CRUZADO EL LAGO");
+			Thread.sleep(100);
+			
+		} catch (Exception e) {
+			
+		}
+	}
+
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------//
+
+/**
+ * Barca: - Debe ser el objeto compartido entre todos los hilos. - Sólo tiene
+ * una plaza libre (además de la del propio barquero). - Cuando sube un
+ * peregrino en su interior, se anota su identificador.
  */
 class Barca {
-	
-}
 
+	private int haySitio = 0;
 
-/** Barquero:
-	- Es quien conduce la Barca y debe esperar a que haya algún peregrino para comenzar su viaje.
-	- En el trayecto de ida tarda 0,5 segundos y al terminar informa por consola del peregrino 
-	que ha llegado al otro lado del lago.
-	- En el trayecto de vuelta tarda 0,1 segundos, ya que vuelve vacío.
- */
-class Barquero {
-	
+	public Barca() {
+
+	}
+
+	public synchronized void empezarviaje() {
+
+		while (haySitio != 0) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		haySitio++;
+		//System.out.println("SE HA ALIMENTADO : " + haySitio);
+		notifyAll();
+
+	}
+
+	public synchronized void iniciarViaje() {
+
+		while (haySitio == 0) { 
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		haySitio--;
+		//System.out.println("SE HA COMIDO: " + haySitio);
+		notifyAll();
+
+	}
+
 }
